0e61bd613c8ead2c5c4840bd3e8da5f0d612b566
dennisthemenace2 <your_email@example.com>
2017-01-29 20:02:43 +0100

    added

--- gpHist/R/downhillsimplex.R
+++ gpHist/R/downhillsimplex.R
@@ -60,12 +60,18 @@ downhillsimplex = function(fc,N,bp=NULL,lower=0,upper=1,it=1000,tol=1e-10){
     
     ord = order(bp[,N+1])
     bp = bp[ ord,] ##order
+
+    print('bp');
+    print(bp);
+    
     if(N>1){
       m = colMeans(bp[1:N,1:N])
     }else{
       m = sum(bp[,1])
     }
     
+    print('\n\n\nm');
+    print(m);
     #  print(bp[1,N+1]-bp[N+1,N+1] )
     if(abs(bp[1,N+1]-bp[N+1,N+1]) < tol){
       cat(c('converged after:',i,' iterations. value:', abs(bp[1,N+1]-bp[N+1,N+1]) ,'\n' ) )
@@ -73,15 +79,15 @@ downhillsimplex = function(fc,N,bp=NULL,lower=0,upper=1,it=1000,tol=1e-10){
     }
     
     r =(1+alpha)*m - alpha*bp[N+1,1:N]
-    # print('querry r')
-    #  print(r)
+    print('querry r')
+    print(r)
     r = chkBound(r,lower,upper )
     
     np = fc(r)
     if(np<bp[1,N+1]){ ##better
       e =(1+gamma)*m -gamma*bp[N+1,1:N]
-      #   print('querry e')
-      #    print(e)
+         print('querry e')
+         print(e)
       ###check bounderys
       e = chkBound(e,lower,upper )
       
@@ -93,28 +99,46 @@ downhillsimplex = function(fc,N,bp=NULL,lower=0,upper=1,it=1000,tol=1e-10){
         bp[N+1,1:N] = r
         bp[N+1,N+1] = np
       }
+      print('bp')
+      print(bp)
       next; ##jump back
     }
     
     if(np<bp[N,N+1]){
       bp[N+1,1:N] = r
       bp[N+1,N+1] = np
+      
+      print('bp')
+      print(bp)
       next;
     }
     
+    print('np')
+    print(np)
     if(np < bp[N+1,N+1]){ ##np better
-      h = np
+      print('np is better')
+      h = r
+      print("np\n");
+      print(h);
     }else{
+      print('n+1 is better')
       h =  bp[N+1,1:N]
       np = bp[N+1,N+1]
     }
     cp = beta*m+(1-beta)*h
-    # print('querry cp')
-    #  print(cp)
+    print('querry cp before chkbound')
+    print(cp)
     cp = chkBound(cp,lower,upper )
+    print('querry cp')
+    print(cp)
     nc = fc(cp)
     if(nc< bp[N+1,N+1]){ ##c is better
       bp[N+1,1:(N+1) ] = c(cp,nc)
+      
+      
+      print('c is better bp')
+      print(bp)
+      
       next;
     }
     for(k in 2:(N+1) ){ ##shrink
@@ -127,7 +151,11 @@ downhillsimplex = function(fc,N,bp=NULL,lower=0,upper=1,it=1000,tol=1e-10){
       #    print(bp[k,1:N])
       #    print(bp[k,N+1])
     }
+   
     
+    print('bp last')
+    print(bp)
+     
   }
   #  cat(c('iterations:',i,'\n') )
   bp
@@ -158,26 +186,31 @@ downhillsimplexCCC = function(fc,nParams,bp=NULL,lower=0,upper=1,it=1000,tol=1e-
   
   if(is.null(bp)){
     #bp = matrix(runif( (N+1)*N,0,5),nrow=N+1,ncol=N )
-    bp = matrix(0,nrow=N+1,ncol=N )
-    for(i in 1:N){
-      bp[,i] = runif( (N+1),lower[i],upper[i])
+    bp = matrix(0,nrow=nParams+1,ncol=nParams )
+    for(i in 1:nParams){
+      bp[,i] = runif( (nParams+1),lower[i],upper[i])
     }
   }else{
-    if(nrow(bp) != N+1){
+    if(nrow(bp) != nParams+1){
       print('solution points wrong dimension! your input will be replaced')
-      bp = matrix(runif( (N+1)*N,0,5),nrow=N+1,ncol=N )
+      bp = matrix(runif( (nParams+1)*nParams,0,5),nrow=nParams+1,ncol=N )
     }
-    if(ncol(bp)>N){
+    if(ncol(bp)>nParams){
       print('solution points dosnt need function value.')
-      bp = bp[,1:N]
+      bp = bp[,1:nParams]
     }
   }
   
-  
+  #void Cdownhillsimplex(SEXP func,SEXP sys,int* nParams,double* bp,double* lower,double* upper, double alpha= 1,double gamma=2,double beta=0.5,double sigma=0.5,unsigned int it=1000,double tol=1e-10){
+    
+    
   output =.C("Cdownhillsimplex",
-             func = fc,sys.frame(),
+             func = fc,
+             frame = sys.frame(),
              nParams  = as.integer(nParams),
-             result = as.double(bp)
+             result = as.double(bp),
+             lower = as.double(lower),
+             upper = as.double(upper)
   )
   
   return(T);
--- gpHist/src/RInterface.cc
+++ gpHist/src/RInterface.cc
@@ -8,8 +8,10 @@
 
 
 #include <iostream> // For cout, cerr, endl, and flush
-
-void fastMultiply(const RMat& X,RMat& Y, RMat& result, RMat& orders, double sigma){
+#include <algorithm>
+#include <stdlib.h>
+/*
+void fastMultiplyBACK(const RMat& X,RMat& Y, RMat& result, RMat& orders, double sigma){
   
 //  RMat prev(Y.NumRows ,1);
   result.SetZero();
@@ -44,19 +46,69 @@ void fastMultiply(const RMat& X,RMat& Y, RMat& result, RMat& orders, double sigm
   result += sigma*Y ;
   
 }
+*/
+
+void fastMultiply( RMat& X,RMat& Y, RMat& result, RMat& orders, double sigma){
+  
+  //  RMat prev(Y.NumRows ,1);
+  result.SetZero();
+  unsigned int NR = X.NumRows();
+  
+  double *result_ptr = result.getValuesPtr();
+  
+  RMat YsumMat(NR,1);
+  double*  YsumMat_ptr = YsumMat.getValuesPtr();
+  double* Y_ptr = Y.getValuesPtr();
+   
+  for(unsigned int i=1;i<=X.NumCols();++i){
+    
+    unsigned int idx = 0;
+    double matcount = 0;
+
+    
+    double* order_prt = orders.getColPtr(i);
+    double* idx_prt = order_prt;
+    double* X_ptr = X.getColPtr(i);
+    
+    
+    //calc Y_sum first
+    double Ysum=0;
+    for(int x=NR-1;x>=0;--x ){ // estimate Y.sum(k,NR); 
+      Ysum += Y_ptr[(int) order_prt[x]-1 ]; //Y(orders(x,i) ,1);
+      YsumMat_ptr[x] = Ysum;
+    }
+    //YsumMat.Print("Ysummat");
+    
+    for(unsigned int k=0;k<NR-1;++k ){ // estimate prev
+      idx =  *(idx_prt++); //orders(k,i);
+      idx -=1;
+      
+      result_ptr[idx] +=  matcount + X_ptr[idx]*YsumMat_ptr[k]; // Ysum
+      matcount = matcount + X_ptr[idx] * Y_ptr[idx];
+      
+    }
+    idx = *(idx_prt++);//orders(NR,i);
+    idx -=1; 
+  //  std::cout<<"Y(idx,1)"<<Y(idx,1)<<std::endl;
+    
+    result_ptr[idx] += matcount + X_ptr[idx] * YsumMat_ptr[NR-1];
+    
+  }
+  result += sigma*Y ;
+}
 
 
 
-bool conjgradFP(const RMat& A, RMat& b,RMat& x ,RMat& orders,double sigma, unsigned int max_iterations=1000 ) {
+bool conjgradFP( RMat& A, RMat& b,RMat& x ,RMat& orders,double sigma, unsigned int max_iterations=1000 ) {
 
   RMat res( x.NumRows(),1 );
 
   fastMultiply(A,x,res,orders,sigma); //A%*%x;
 
-  RMat r= b-res;// #A%*%x;
+  RMat r= b-res;// 
   RMat p=r;
   
-  double rsold=r.SquareSum();//t(r)%*%r
+  double rsold=r.SquareSum();//t(r)%*%r 
     
   for (unsigned int i=1;i<=max_iterations;++i){
     fastMultiply(A,p,res,orders,sigma);// res =  A%*%p;
@@ -91,11 +143,11 @@ bool conjgradFP(const RMat& A, RMat& b,RMat& x ,RMat& orders,double sigma, unsig
   return false;
 }
 // power method
-double getEigen(const RMat& A,RMat &b,RMat& orders,double sigma){
+double getEigen( RMat& A,RMat &b,RMat& orders,double sigma){
   double last = 0;
   RMat res( b.NumRows(),1 );
   
-  for(unsigned int i=0;i<10000;++i){
+  for(unsigned int i=0;i<10000;++i){ // or give it a max iterations parameter
     fastMultiply(A,b,res,orders,sigma); //A%*%b;
     double norm = sqrt(res.SquareSum()) ;
   //  res.Print();
@@ -103,7 +155,7 @@ double getEigen(const RMat& A,RMat &b,RMat& orders,double sigma){
     b = res;
     b /= norm;
   //  b.Print();
-    if(abs(last-norm)<1e-10 ){
+    if(fabs(last-norm)<1e-10 ){
         break;
     }
     last= norm;
@@ -114,19 +166,28 @@ double getEigen(const RMat& A,RMat &b,RMat& orders,double sigma){
 // two helper functions for predictions
 double sumOrdered(RMat& alpha,RMat&orders,int start,int end,int col){
   double result = 0;
+  double *orders_ptr = orders.getColPtr(col)+(start-1);
+  double *alpha_ptr = alpha.getValuesPtr();
+  
   for(int i= start;i<=end;++i){
-    int idx = orders(i,col);
-    result += alpha(idx,1);
+    int idx = *(orders_ptr++);
+    idx -=1; // or set pointer to invalid location and skip this ?
+    result += alpha_ptr[idx];
   }
   return result;
 }
   
 double sumSquareOrdered(RMat& X,RMat&orders,int start,int end, int col){
   double result = 0;
+  double *X_ptr = X.getColPtr(col);
+  double *orders_ptr = orders.getColPtr(col)+(start-1);
+  
   for(int i= start;i<=end;++i){
-    int idx = orders(i,col);
+    int idx = *(orders_ptr++);
+    idx -=1;
     //  std::cout<<"sumordidx"<<idx<<std::endl;
-    result += X(idx,col)*X(idx,col);
+    double val = X_ptr[idx];
+    result += val*val;
   }
   
   return result;
@@ -134,16 +195,60 @@ double sumSquareOrdered(RMat& X,RMat&orders,int start,int end, int col){
     
 double sumOrdered(RMat& X,RMat& alpha,RMat&orders,int start,int end, int col){
   double result = 0;
+  double *alpha_ptr = alpha.getValuesPtr();
+  double *X_ptr = X.getColPtr(col);
+  double *orders_ptr = orders.getColPtr(col)+(start-1);
+  
   for(int i= start;i<=end;++i){
-    int idx = orders(i,col);
+    int idx = *(orders_ptr++);
+    idx -=1;
   //  std::cout<<"sumordidx"<<idx<<std::endl;
-    result += X(idx,col)*alpha(idx,1);
+    result += X_ptr[idx]*alpha_ptr[idx];
   }
   
   return result;
 }
 
 int findIdx(RMat& X,RMat& orders,double value,int col){
+ // try intersection...
+  double *orders_ptr = orders.getColPtr(col); // pointer to column
+  double *X_ptr = X.getColPtr(col); // pointer to column
+  
+  int min = 0;
+  int max = X.NumRows()-1;
+  int mid; 
+//  std::cout<<"max"<<max<<std::endl;
+//  std::cout<<"mid"<<mid<<std::endl;
+  do{
+    mid = (min+max)/2;
+//    std::cout<<"mid"<<mid<<std::endl;
+    
+//    std::cout<<" X_ptr[(int) orders_ptr[mid] ] "<< X_ptr[(int) orders_ptr[mid] -1]<<std::endl;
+    if(X_ptr[(int) orders_ptr[mid]-1 ] >value  ){ // go left
+      max = mid;
+//      std::cout<<"go left max "<<max<<std::endl;
+    }else{ // go right should i consider equal 
+      min = mid;
+//      std::cout<<"go right min "<<min<<std::endl;
+    }
+
+  }while(max-min >1);
+
+  if( value <= X_ptr[(int) orders_ptr[min]-1 ] ){
+ //   std::cout<<"value smaller "<<std::endl;
+    mid = min;//-1;
+  }else{
+    mid = max;
+    if(value>X_ptr[(int) orders_ptr[max]-1 ]){
+  //    std::cout<<"value bigger "<<std::endl;
+      mid = max+1;
+    }
+  }
+  
+  return mid;
+}
+
+/*int findIdx(RMat& X,RMat& orders,double value,int col){
   int k =1;
   for(;k<=X.NumRows();++k){ // this is linear this suckz
     int idx = orders(k,col);
@@ -153,7 +258,8 @@ int findIdx(RMat& X,RMat& orders,double value,int col){
   }
   
   return k-1;
-}
+}*/
+
 void CppHistVariance(double* result,double numRows,double numCols,double numRows2,double numCols2,double* mat1,double* mat2,double *mat3,double lambda,double sigma,double* orders){
   RMat  vMatX(mat1,numRows,numCols);
   RMat  vMatPred(mat2,numRows2,numCols2);
@@ -290,11 +396,11 @@ void CppHist(double *result,
     
    double logdetM =  (t1 * t2).ScalarProd(t3);
    
- //  std::cout<<"logdet:"<<logdetM<<std::endl;
+  // std::cout<<"logdet:"<<logdetM<<std::endl;
    
-   *logmarginal= 0.5*vMatY.ScalarProd( vMatAlphas ) + logdetM/2 + (numRows/2.0)*log(2.0*PI);
-//   std::cout<<"   vMatY.ScalarProd( vMatAlphas ) :"<<   vMatY.ScalarProd( vMatAlphas ) <<std::endl;
-//   std::cout<<"    numRows/2*log(2*PI):"<<   (numRows/2.0)*log(2.0*PI)<<std::endl;
+   *logmarginal= 0.5*vMatAlphas.ScalarProd(vMatY)+ logdetM/2 + (numRows/2.0)*log(2.0*PI);
+  // std::cout<<"   vMatY.ScalarProd( vMatAlphas ) :"<<   vMatY.ScalarProd( vMatAlphas ) <<std::endl;
+   //std::cout<<"    numRows/2*log(2*PI):"<<   (numRows/2.0)*log(2.0*PI)<<std::endl;
    
   // std::cout<<"approxmarginal:"<<*logmarginal<<std::endl;
 
@@ -315,6 +421,56 @@ void Cmtest(double *result,
 }
 
 
+int compare ( const void *pa, const void *pb ){
+  const double *a = (double*) pa;
+  const double *b = (double*) pb;
+  
+  if(a[0]  > b[0] ) return 1;
+  if(a[0] == b[0])  return 0;
+  if(a[0]  < b[0])  return -1;
+}
+
+void sortIdx(RMat &vMatValues){
+
+ // vMatValues.Print("before sort");
+  qsort(vMatValues.getValuesPtr(), vMatValues.NumCols() ,2*sizeof(double),compare);
+ // vMatValues.Print("after sort");
+}
+
+void chkBound(RMat& params,double* lower, double* upper){
+  
+  double *params_ptr = params.getValuesPtr();
+  for(unsigned int i=0;i<params.NumRows();++i){
+    if(params_ptr[i]<lower[i]){
+      params_ptr[i] = lower[i];
+    }else if(params_ptr[i]>upper[i]){
+      params_ptr[i] = upper[i];
+    } 
+  }
+  
+}
+
+double dummy(RMat& params,SEXP function_call,SEXP environment,double* input){
+ 
+  //double
+  //for( int i = 0;i<params.NumRows();++i){
+  //  input[i] =;
+  //}
+  memcpy(input,params.getValuesPtr(),params.NumRows()*sizeof(double));
+  
+  SEXP s_fval;
+  PROTECT(s_fval = eval(function_call, environment));
+  if(length(s_fval) != 1) {
+    error("Function should return error to minimize");
+  }
+  double *err = REAL(s_fval);
+ // std::cout<<"res:"<< *err <<std::endl;
+  double ret = *err;
+  UNPROTECT(1); /* s_fval */
+
+  return ret ;
+}
+
 extern "C" {
     void CgpHist(double *result,double *mat1,int *numRows,int *numCols, double *mat2,int *numRows2,int *numCols2,double *sigma,double *orders,double* logmarginal,double *lambda){
       CppHist(result,*numRows,*numCols,*numRows2,*numCols2,mat1,mat2,*sigma,orders,logmarginal,lambda);
@@ -328,7 +484,7 @@ extern "C" {
     CppHistVariance(result,*numRows,*numCols,*numRows2,*numCols2,mat1,mat2,mat3,*lambda,*sigma,orders);
   }
 
-  void Cdownhillsimplex(SEXP func,SEXP sys,int* nParams,double* bp){
+  void Cdownhillsimplex(SEXP func,SEXP sys,int* nParams,double* bp,double* lower,double* upper,int *resPtr, double *alpha,double *gamma,double *beta,double *sigma,unsigned int *it,double *tol){
     SEXP arg;
     SEXP environment;
     
@@ -337,17 +493,17 @@ extern "C" {
  // memcpy(REAL(arg), 5, (*nParams) * sizeof(double));
     double *input = REAL(arg);
  
-    for( int i = 0;i<*nParams;++i){
-      input[i] =i;
-    }
+    //for( int i = 0;i<*nParams;++i){
+    //  input[i] =i;
+  //  }
     
     SEXP function_call;
     PROTECT(function_call = lang2(func, arg));
     
-    SEXP s_fval;
-    PROTECT(s_fval = eval(function_call, environment));
+    //SEXP s_fval;
+   // PROTECT(s_fval = eval(function_call, environment));
     
-    if(length(s_fval) != 1) {
+   /* if(length(s_fval) != 1) {
       error("Function should return error to minimize");
     }
     double *err = REAL(s_fval);
@@ -356,11 +512,171 @@ extern "C" {
     UNPROTECT(4); /* s_fval */
         
         
+        /////////
+        
+        int Np1 = (*nParams)+1;
+        RMat  vMatbp(bp,*nParams,Np1); //parameters but make downwords
+        RMat  vMatValues( 2,Np1); //functiosn values..next row idx
+        
+        RMat  m(*nParams ,1); //mean over params
+        RMat  r(*nParams ,1); //new parameters
+        RMat  e(*nParams ,1); //new parameters
+        
+//  RMat  vMatIdx( (*nParams)+1,1); //functiosn values..
+        
+        double fcRes;
+        for(int i=1;i<=Np1;++i){
+          RMat point(vMatbp.getColPtr(i),*nParams ,1); // get vector of params
+          fcRes = dummy(point,function_call,environment, input);
+          vMatValues(1,i) = fcRes;
+          //res =c(res, fc(bp[k,]) )
+        }
+        
+        for(unsigned i=1;i<=vMatValues.NumCols();++i ){ // set idx...
+          vMatValues(2,i)=i;
+        }
+        
+        
+        //vMatValues.Print("values");
+       // std::cout<<"iterations:"<<*it<<std::endl;
+        
+        for(unsigned int i=0;i<*it;++i){
+          // sort my function valiues
+          
+          sortIdx(vMatValues);
+          // check convergenc
+        // std::cout<<"convergence value"<< fabs(vMatValues(1,1)-vMatValues(1,Np1)) <<std::endl;
+          
+          
+          if(fabs( vMatValues(1,1) -vMatValues(1,Np1) )  < *tol){
+          //  cat(c('converged after:',i,' iterations. value:', abs(bp[1,N+1]-bp[N+1,N+1]) ,'\n' ) )
+            std::cout<<"converged after:"<<i<<" iterations."<< fabs(vMatValues(1,1)-vMatValues(1,Np1))<<std::endl;
+            break;
+          }
+          
+        //  vMatbp.Print("vMat");
+          //have to do stuff because not the last solution...
+          int idx ;
+          m.SetZero();
+          for(int x=1;x<vMatbp.NumCols();++x){
+            idx =  (int) vMatValues(2,x);
+            RMat setup(vMatbp.getColPtr(idx), (*nParams) ,1);
+            m += setup;
+          }
+          //m = vMatbp.RowSums();
+          m /= (*nParams); // mean
+          
+          m.Print("\n\n\nm");
+          
+          idx =(int) vMatValues(2,Np1);
+       //   m.Print("mean");
+         // std::cout<<"calculated mean"<<std::endl;  
+            
+          RMat point(vMatbp.getColPtr(idx),*nParams ,1); // get vector of params
+       //   point.Print("point...");
+          r =(1.+(*alpha)) *m - point*(*alpha) ;//bp[N+1,1:N]
+          r.Print("r");
+          chkBound(r, lower, upper);
+          
+      //    std::cout<<"after chk bound r"<<std::endl;  
+      //    r.Print("R\n");
+          //np = fc(r) //querry point
+          fcRes =dummy(r,function_call,environment, input);;
+            
+           if(fcRes<vMatValues(1,1)){ //##better
+              e =(1+(*gamma)) *m -point* (*gamma);
+             std::cout<<"after calc e"<<std::endl;  
+             e.Print("e\n");
+              chkBound(e,lower,upper );
+               
+               double fcRes2 = dummy(e,function_call,environment, input);;
+               
+               if(fcRes2<fcRes){ //e better
+                 point = e;
+                 vMatValues(1,Np1) = fcRes2 ;
+               }else{
+                 point = r;
+                 vMatValues(1,Np1) = fcRes ;
+               }
+               vMatValues.Print("vMatValues\n");
+               vMatbp.Print("vMatbp\n");
+               
+               continue;
+           }
+           
+           
+           if(fcRes<vMatValues(1,*nParams)){
+            // idx =(int) vMatValues(2,*nParams);// set second worst solution
+           //  point.setValuesPtr(vMatbp.getColPtr(idx));
+             
+             point = r;
+             vMatValues(1,Np1) = fcRes ;
+             
+             vMatValues.Print("set r vMatValues\n");
+             vMatbp.Print("vMatbp\n");
+             
+             continue;
+           }
+           std::cout<<"fc res"<< fcRes<<std::endl;
+           std::cout<<"vMatValues(1,Np1) res"<< vMatValues(1,Np1)<<std::endl;
+           
+           
+           if(vMatValues(1,Np1)<fcRes){ 
+             e = (*beta) *m+(1.- *beta)*point;
+             std::cout<<"n+1 is better"<<std::endl;
+           }else{
+             e = (*beta) *m+(1.- *beta)*r;
+             std::cout<<"fc is better"<<std::endl;
+             r.Print("my r");
+           }
+           
+           e.Print("cp before ckbound");
+           
+           chkBound(e,lower,upper );
+           fcRes = dummy(e,function_call,environment, input);
+           e.Print("get cp query");
+           if(fcRes< vMatValues(1,Np1)){
+               point = e;
+               vMatValues(1,Np1)= fcRes ;
+               vMatValues.Print("vMatValues accteo cp");
+               vMatbp.Print("vMatbp");
+               
+               continue;
+           }
+           
+           // idx =(int) vMatValues(2,*nParams);// set second worst solution
+           //  point.setValuesPtr(vMatbp.getColPtr(idx));
+           
+           idx = (int) vMatValues(2,1);
+           RMat best(vMatbp.getColPtr(idx),*nParams ,1); 
+           
+           for(int k=2;k<= vMatbp.NumCols();++k){ 
+             idx = (int) vMatValues(2,k);
+             point.setValuesPtr(vMatbp.getColPtr(idx));
+             
+             e = best+ (*sigma) *( point -best);
+             chkBound(e,lower,upper );
+             point = e;
+             fcRes = dummy(e,function_call,environment, input);
+             vMatValues(1,k)= fcRes;
+           }
+           
+           vMatValues.Print("vMatValues last");
+           vMatbp.Print("vMatbp");
+           
+
+        }
+        *resPtr =vMatValues(2,1) ;
+        
+    UNPROTECT(3);
+   
     //Cdownhillsimplex SEXP
-    std::cout<<"OK"<<std::endl;
+   // std::cout<<"OK"<<std::endl;
   }
   
-  
+
+     
+       
   void CgpfMult(double *result,double *mat1,int *numRows,int *numCols, double *mat2,int *numRows2,int *numCols2,double *sigma,double *orders)
   {
     Cmtest(result,numRows,numCols,numRows2,numCols2,mat1,mat2,sigma,orders);  
