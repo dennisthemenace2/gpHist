19a4b6dc0cfe1da9dd9795f7157be1a1bab9b781
dennisthemenace2 <your_email@example.com>
2017-01-29 20:04:02 +0100

    added

---
+++ gpHist/src/RMat.cc
@@ -0,0 +1,453 @@
+#include "RMat.h"   // For RMat class
+#include <limits>   // For NaN
+#include <iostream> // For cout, cerr, endl, and flush
+#include <assert.h> // For assert
+
+#if COMPILE_WITH_R
+#include "R.h"      // For Rprintf
+#endif
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::flush;
+
+//- Constructor #1
+//
+RMat::RMat() : mValues(0), mNumRows(0),
+mNumCols(0), mNaN(std::numeric_limits<double>::quiet_NaN()),allocated(false)
+{
+}
+
+//- Constructor #2
+RMat::RMat(int x, int y) : mNumRows(x),mNumCols(y),allocated(false),mValues(0),
+ mNaN(std::numeric_limits<double>::quiet_NaN())
+{
+    AllocateMemory(x,y);
+}
+
+//- Constructor #3
+//  This constructor will be used to point an RMat object to the memory
+//  location provided by R, via the '.C()' function.
+//
+RMat::RMat(double *vElements, int x, int y) :
+mNaN(std::numeric_limits<double>::quiet_NaN()),allocated(false)
+{
+    mValues  = vElements;
+    mNumRows = x;
+    mNumCols = y;
+}
+
+//- Constructor #4 (copy constructor)
+//
+RMat::RMat(const RMat &vRhs) : mValues(0), mNumRows(0),
+mNumCols(0), mNaN(std::numeric_limits<double>::quiet_NaN())
+{
+  AllocateMemory(vRhs.NumRows(),vRhs.NumCols());
+  
+  memcpy(mValues,vRhs.mValues,mNumRows*mNumCols*sizeof(double));
+  
+//  for ( int i=0; i<mNumRows*mNumCols; i++){
+//    mValues[i] = vRhs.mValues[i];
+//  }
+
+}
+
+//- Destructor
+//
+RMat::~RMat()
+{
+    DeallocateMemory();
+}
+
+void RMat::SetZero(){
+  if(mValues!= NULL){
+    memset( mValues,0,mNumCols*mNumRows*sizeof(double) );
+  }
+}
+//- Assignment operator #1
+//
+RMat&
+RMat::operator= (const RMat &vRhs)
+{
+ 
+    if(mNumCols != vRhs.mNumCols || mNumRows!=vRhs.mNumRows){
+      cout << "DeallocateMemory\n";
+      DeallocateMemory();
+      AllocateMemory(vRhs.NumRows(),vRhs.NumCols());
+    }
+    for ( int i=0; i<mNumRows*mNumCols; i++){
+      mValues[i] = vRhs.mValues[i];
+    }
+    return (*this);
+}
+
+//- Assignment operator #2
+//
+RMat&
+RMat::operator= (double vValue)
+{
+  //  for (int i=0; i<mNumRows*mNumCols; i++){
+//        mValues[i] = vValue;
+//    }
+    memset( mValues,vValue,mNumCols*mNumRows*sizeof(double) );
+    return (*this);
+}
+
+
+RMat&
+RMat::operator-= (const RMat &vRhs)
+{
+  assert(mNumCols==vRhs.mNumCols);
+  assert(mNumRows==vRhs.mNumRows);
+    
+  for (int i=0; i<mNumRows*mNumCols; i++){
+    mValues[i] -= vRhs.mValues[i];
+  }
+  return (*this);
+}
+
+//- Subtraction operator
+//
+
+RMat
+RMat::operator- (const RMat &vRhs)
+{
+  assert(mNumCols==vRhs.mNumCols);
+  assert(mNumRows==vRhs.mNumRows);
+  
+  RMat vProduct(mNumRows,mNumCols);
+  for (unsigned int i=0; i<mNumRows*mNumCols; i++){
+    vProduct.mValues[i] = mValues[i] - vRhs.mValues[i];
+  }
+  return vProduct;
+}
+
+RMat
+RMat::operator/ (const double vRhs)
+{
+  RMat vProduct(mNumRows,mNumCols);
+  for (unsigned int i=0; i<mNumRows*mNumCols; i++){
+    vProduct.mValues[i] = mValues[i] / vRhs;
+  }
+  return vProduct;
+}
+
+//Add
+RMat
+  RMat::operator+ (const RMat &vRhs)
+  {
+    assert(mNumCols==vRhs.mNumCols);
+    assert(mNumRows==vRhs.mNumRows);
+    
+    RMat vProduct(mNumRows,mNumCols);
+    for (unsigned int i=0; i<mNumRows*mNumCols; i++){
+      vProduct.mValues[i]= mValues[i]+vRhs.mValues[i]; 
+    }
+    return vProduct;
+  }
+
+RMat&
+RMat::operator+= (const double vRhs)
+{
+  for (unsigned int i=0; i<mNumRows*mNumCols; i++){
+    mValues[i] +=  vRhs;
+  }
+    
+  return (*this);
+}
+
+RMat&
+RMat::operator*= (const double vRhs)
+{
+  for (unsigned int i=0; i<mNumRows*mNumCols; i++){
+    mValues[i] *=  vRhs;
+  }
+  
+  return (*this);
+}
+
+RMat&
+RMat::operator/= (const double vRhs)
+{
+  for (unsigned int i=0; i<mNumRows*mNumCols; i++){
+    mValues[i] /=  vRhs;
+  }
+    
+  return (*this);
+}
+
+RMat&
+  RMat::operator+= (const RMat &vRhs)
+  {
+    assert(mNumCols==vRhs.mNumCols);
+    assert(mNumRows==vRhs.mNumRows);
+    
+    for (int i=0; i<mNumRows*mNumCols; i++){
+      mValues[i] += vRhs.mValues[i];
+    }
+    return (*this);
+  }
+
+double 
+RMat::Sum(){
+    double result=0;
+    for ( int i=0; i<mNumRows*mNumCols; ++i){
+      result+= mValues[i]; 
+    }
+    return result;
+}
+
+
+double RMat::ScalarProd(const RMat &vRhs){
+//  assert(mNumCols==vRhs.mNumRows); // altough I could skip this check and abuse this function
+//  assert(mNumRows==vRhs.mNumCols);
+//  assert(mNumRows==1 && vRhs.mNumCols==1);
+  
+  double result=0;
+  for ( int i=0; i<mNumRows*mNumCols; ++i){
+    result+= mValues[i]*vRhs.mValues[i]; 
+  }
+  
+  //double result=0;
+  //for ( int i=1; i<=vRhs.mNumCols; ++i){
+  //  double* row = vRhs.getColPtr(i);
+  //  for ( int k=0; k<vRhs.mNumRows; ++k){
+    //  result+= mValues[k]*row[k]; 
+   // }
+  //}
+  return result;
+}
+
+double 
+RMat::SquareSum(){// or call this square norm, could also call ScalarProd
+  double result=0;
+  for ( int i=0; i<mNumRows*mNumCols; ++i){
+    result+= mValues[i]*mValues[i]; 
+  }
+  
+  return result;
+}
+
+// THIS IS a hack
+double* RMat::getColPtr( int col ){
+  return &mValues[ (col-1)*mNumRows];
+}
+
+/*
+//RMat RMat::operator/(double vLhs, const RMat &vRhs){
+  RMat vProduct(vRhs.mNumRows,vRhs.mNumCols);
+  for ( int i=0; i<vRhs.mNumRows*vRhs.mNumCols; i++){
+    vProduct.mValues[i] = vLhs/vRhs.mValues[i];
+  }
+  
+  return vProduct;
+}
+*/
+
+//- Multiplication operator
+//
+RMat
+RMat::operator* (const RMat &vRhs) // maybe make this faster..
+{
+    assert(mNumCols==vRhs.mNumRows);
+    RMat vProduct(mNumRows,vRhs.NumCols());
+    for ( int i=1; i<=mNumRows; i++)
+    {
+        for ( int j=1; j<=vRhs.NumCols(); j++)
+        {
+            for ( int k=1;k<=mNumCols;k++)
+            { 
+                vProduct(i,j)+=(*this)(i,k)*vRhs(k,j); 
+            } 
+        }
+    }
+    return vProduct;
+}
+
+RMat RMat::ColSums(){
+  RMat vProduct(mNumCols,1);
+  for(int j=0;j<mNumCols;++j){
+    for(int i=0;i<mNumRows;++i){
+      vProduct.mValues[j] += mValues[ (j)*mNumRows+(i)];
+    }
+  }
+  return vProduct;
+}
+
+RMat RMat::RowSums(){
+  RMat vProduct(mNumRows,1);
+ /* for(int i=0;i<mNumRows;++i){
+    for(int j=0;j<mNumCols;++j){
+      vProduct.mValues[i] += mValues[ (j)*mNumRows+(i)];
+    }
+ }*/
+ for(int j=0;j<mNumCols;++j){
+  for(int i=0;i<mNumRows;++i){
+     vProduct.mValues[i] += mValues[ (j)*mNumRows+(i)];
+   }
+ }
+  return vProduct;
+}
+
+RMat
+RMat::operator* (const double vRhs)
+{
+    
+  RMat vProduct(mNumRows,mNumCols);
+  for ( int i=0; i<mNumRows*mNumCols; i++){
+       vProduct.mValues[i] =  mValues[i]* vRhs;
+  }
+  return vProduct;
+}
+
+//- Element access
+//
+double&
+RMat::operator()( int x, int y) const
+{
+    //- Basic range checks.
+    //
+    if ( ( x < 1 ) || ( x > mNumRows )
+      || ( y < 1 ) || ( y > mNumCols ) )
+    {
+         cerr << "Rmat::operator(): range error!" << endl;
+         return (double &) mNaN;
+    }
+    
+    //- Offset is ((y-1)*mNumRows)+(x-1)
+    //  rather than ((x-1)*mNumCols)+(y-1)
+    //  because we want to organize memory the same
+    //  way as it is in an R matrix.  R is column-major.
+    //  Reference:
+    //  http://en.wikipedia.org/wiki/Row-major_order#Column-major_order
+    //
+    return mValues[((y-1)*mNumRows)+(x-1)];
+}
+
+
+//- Transpose
+//
+RMat
+RMat::Transpose() // this should be slow...
+{
+    RMat vTranspose(mNumCols,mNumRows);
+    for (int i=1; i<=mNumRows; i++)
+    {
+        for (int j=1; j<=mNumCols; j++)
+        {
+            vTranspose(j,i) = (*this)(i,j); 
+        }
+    }
+    return vTranspose;
+}
+
+//- Print #1
+//
+void
+RMat::Print()
+{
+    for ( int i=1; i<=mNumRows; i++)
+    {
+        for ( int j=1; j<=mNumCols; j++)
+        {
+            cout << (*this)(i,j) << "  ";
+        }
+        cout << endl;
+    }
+}
+
+//- Print #2
+//
+void
+RMat::Print(const char *vString)
+{
+    cout << vString;
+    Print();
+}
+
+#if COMPILE_WITH_R
+//- RPrint #1
+//
+void
+RMat::RPrint()
+{
+    for ( int i=1; i<=mNumRows; i++)
+    {
+        for ( int j=1; j<=mNumCols; j++)
+        {
+            Rprintf("%g  ",(*this)(i,j));
+        }
+        Rprintf("\n");
+        R_FlushConsole();
+        R_ProcessEvents();
+    }
+}
+
+//- RPrint #2
+//
+void
+RMat::RPrint(const char *vString)
+{
+    Rprintf("%s",vString);
+    RPrint();
+}
+#endif
+
+//- Get number of rows
+//
+int
+RMat::NumRows() const
+{
+    return mNumRows;
+}
+
+//- Get number of columns
+//
+int
+RMat::NumCols() const
+{
+    return mNumCols;
+}
+
+//- Allocate memory
+//  Initialize all entries to 0.
+//
+bool             
+RMat::AllocateMemory( int x, int y)
+{
+    DeallocateMemory();
+    try {
+        mValues  = new double [x*y];
+    }
+    catch (...) {
+        cerr << "Rmat::AllocateMemory(): unable to allocate memory for "
+             << x << " by " << y << " matrix!" << endl;
+        return false;
+    }
+
+    mNumRows = x;
+    mNumCols = y;
+    allocated=true;
+    SetZero(); // do i realy need to zero this ?
+  /*  for ( int i=1; i<=mNumRows; i++)
+    {
+        for ( int j=1; j<=mNumCols; j++)
+        {
+            (*this)(i,j) = 0.0;
+        }
+    }*/
+  
+    return true;
+}
+
+//- Deallocate memory
+//
+void             
+RMat::DeallocateMemory()
+{
+    if ( mValues != 0 && allocated){ // make sure we dont delete R's memory
+        delete [] mValues;
+        mValues = 0;
+    }
+}
---
+++ gpHist/src/RMat.h
@@ -0,0 +1,94 @@
+#ifndef _RMAT_H
+#define _RMAT_H
+
+#define COMPILE_WITH_R 1
+
+class RMat {
+public:      
+    //- Constructors and destructor.
+    //
+    RMat();                                // Constructor #1
+    RMat( int x,   int y);                    // Constructor #2 for x-by-y matrix.
+    RMat(double *vElements,   int x,  int y); // Constructor #3 for R interface
+    RMat(const RMat &vRhs);                // Constructor #4 (copy constructor)
+    ~RMat();                               // Destructor
+
+    //- Overloaded operators.
+    //
+    RMat& operator= (const RMat &vRhs);     // Assignment #1
+    RMat& operator= (double vValue);        // Assignment #2
+    RMat operator* (const RMat &vRhs);      // Multiplication
+    RMat operator* (const double vRhs);      // Multiplication
+    RMat operator/ (const double vRhs);      // Div
+    RMat operator- (const RMat &vRhs);      // Subtraction
+    RMat& operator-= (const RMat &vRhs);      // Subtraction
+    
+    RMat operator+ (const RMat &vRhs);      // Add#
+    RMat& operator+= (const RMat &vRhs);      // add in place
+    RMat& operator+= (const double vRhs);      // add in place
+    RMat& operator*= (const double);      // multi in place
+    RMat& operator/= (const double);      // multi in place
+    
+    RMat RowSums();
+    RMat ColSums();
+    void SetZero();
+    double* getColPtr( int col );
+    double* getValuesPtr(){return mValues;};
+    void setValuesPtr(double* ptr){mValues=ptr;};
+    
+    friend RMat operator/(double vLhs, const RMat &vRhs){
+      RMat vProduct(vRhs.mNumRows,vRhs.mNumCols);
+      for ( int i=0; i<vRhs.mNumRows*vRhs.mNumCols; i++){
+        vProduct.mValues[i] = vLhs/vRhs.mValues[i];
+      }
+      return vProduct;
+    }
+    friend RMat operator*(double vLhs, const RMat &vRhs){
+      RMat vProduct(vRhs.mNumRows,vRhs.mNumCols);
+      for ( int i=0; i<vRhs.mNumRows*vRhs.mNumCols; i++){
+        vProduct.mValues[i] = vLhs*vRhs.mValues[i];
+      }
+      return vProduct;
+    }
+    
+        
+   // operator double() const {return mValues[0]; }
+    
+    double& operator()( int x,  int y) const; // Element access
+
+    double SquareSum();
+    double Sum();
+    double ScalarProd(const RMat &vRhs);
+      
+    //- Some utility functions.
+    //
+    RMat Transpose();                 // Transpose
+     int NumRows() const;              // Returns number of rows.
+     int NumCols() const;              // Returns number of columns.
+    void Print();                     // Print matrix to stdout
+    void Print(const char *vString);  // Print matrix to stdout with string.
+#if COMPILE_WITH_R
+    void RPrint();                    // Print matrix to R console.
+    void RPrint(const char *vString); // Print matrix to R console with string.
+#endif
+
+    //- A function for deallocating memory is made publicly available because
+    //  I want to not only run test this code in a pure C++ test
+    //  program outside of R, testRMat.cc, but I also want to run this code
+    //  from within R.  In the latter case, I will let R handle memory
+    //  management.  This means that I can't automatically deallocate memory
+    //  in the destructor, which is the usual thing one might do.
+    //  For symmetry, the function for memory allocation is also made publicly
+    //  available, although it didn't have to be.  (Maybe it should be private!)
+    //
+    bool AllocateMemory( int x,  int y); // Allocate memory for x-by-y matrix.
+    void DeallocateMemory();           // Deallocate memory
+
+private:
+    double  *mValues;           // Matrix values
+   int     mNumRows, mNumCols; // Matrix dimensions
+    bool    allocated;
+    double  mNaN;               // Return NaN on bad input
+};
+
+#endif // _RMAT_H defined
